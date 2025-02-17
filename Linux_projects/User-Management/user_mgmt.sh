ee226384c8bdb70bd67745ea4a91ce3f004153c6
Rohit Jadhav <rj6228442@gmail.com>
2025-02-17 16:24:10 +0530

    Added user management shell script for Linux user operations(add, delete, list, modify users)

--- Linux_projects/User-Management/user_mgmt.sh
+++ Linux_projects/User-Management/user_mgmt.sh
@@ -1,1 +1,53 @@
-usermgmt
+bash'''
+#!/bin/bash
+
+#function to add user
+add_user(){
+echo "enter user name: "
+read username
+sudo useradd $username
+echo "Enter password: "
+echo "user $username added"
+}
+
+#function to delete user
+delete_user(){
+echo "who is that: "
+read username
+sudo userdel -r $username
+echo "bye $username"
+}
+
+#function to list all users
+list_users(){
+echo "Listing all users..."
+cat /etc/passwd | cut -d: -f1
+}
+
+#function to modify user details (change passwd)
+modify_user(){
+echo "Enter username: "
+read username
+sudo passwd $username
+echo "Password changed for $username"
+}
+
+#main menu
+echo "User Management System"
+echo "1. Add User"
+echo "2. Delete User"
+echo "3. List all users"
+echo "4. Modify user detail"
+echo "5. Exit"
+
+while true; do
+        read -p "Enter your choice: " choice
+        case $choice in
+                1) add_user ;;
+                2) delete_user ;;
+                3) list_users ;;
+                4) modify_users ;;
+                5) exit ;;
+                *) echo "Invalid choice..!" ;;
+        esac
+done        
