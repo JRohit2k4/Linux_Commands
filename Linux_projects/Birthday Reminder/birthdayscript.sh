362f4ff02db61641a5aab34416c84243eb8ab5a5
Rohit Jadhav <rj6228442@gmail.com>
2025-03-15 23:50:47 +0530

    defined function and created menu loop

--- Linux_projects/Birthday Reminder/birthdayscript.sh
+++ Linux_projects/Birthday Reminder/birthdayscript.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+BIRTHDAY_FILE=birthday.txt
+
+#Search for todays birthdays
+todays_birthday(){
+}
+
+#Search for upcoming birthdays
+upcoming_birthdays(){
+}
+
+#Add new birthday (YYYY-MM-DD) format
+add_birthday(){
+}
+
+
+#Menu loop
+while true; do
+        echo -e "Birthday Reminder"
+        echo "1.Check today's Birthday"
+        echo "2.Upcoming Birthdays in 7 days"
+        echo "3.Add new Birthday (YYYY-MM-DD)"
+        echo "4.Exit"
+
+        read -p "Choose an option" option
+
+        case $option in
+                1)todays_birthday ;;
+                2)upcoming_birthdays ;;
+                3)add_birthday ;;
+                4)echo "Exiting..." ; break ;;
+                *)echo "Invalid option..!" ;;
+        esac
+done
